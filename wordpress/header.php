8e91a36120b3789f04767658f90ca89932d04419
Сергей <serj.elessar@gmail.com>
2022-12-13 14:31:33 +0300

    fix: update header

--- wordpress/header.php
+++ wordpress/header.php
@@ -12,11 +12,18 @@
   <div class="wrapper">
     <header class="header">
       <div class="header__block">
-          <a href="https://www.theblock.co/post/130653/crypto-market-maker-byte-trading-raises-7-million-in-seed-funding" target="_blank" class="header__text">Byte Trading raises $7m in seed funding<img src="<?php echo get_template_directory_uri()?>/images/icons/rocket.png" alt=""></a>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.29289 17.2929L4.58579 18L6 19.4142L6.70711 18.7071L5.29289 17.2929ZM18.7071 6.70711C19.0976 6.31658 19.0976 5.68342 18.7071 5.29289C18.3166 4.90237 17.6834 4.90237 17.2929 5.29289L18.7071 6.70711ZM6.70711 18.7071L18.7071 6.70711L17.2929 5.29289L5.29289 17.2929L6.70711 18.7071Z" fill="#04040C"/>
-            <path d="M8.25 6H18V15.75" stroke="#04040C" stroke-width="2" stroke-linecap="square"/>
-          </svg>
+          <a 
+              href="https://www.theblock.co/post/130653/crypto-market-maker-byte-trading-raises-7-million-in-seed-funding" 
+              target="_blank" 
+              class="header__text"
+          >
+            Byte Trading raises $7m in seed funding
+            <img src="<?php echo get_template_directory_uri()?>/images/icons/rocket.png" alt="">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.29289 17.2929L4.58579 18L6 19.4142L6.70711 18.7071L5.29289 17.2929ZM18.7071 6.70711C19.0976 6.31658 19.0976 5.68342 18.7071 5.29289C18.3166 4.90237 17.6834 4.90237 17.2929 5.29289L18.7071 6.70711ZM6.70711 18.7071L18.7071 6.70711L17.2929 5.29289L5.29289 17.2929L6.70711 18.7071Z" fill="#04040C"/>
+              <path d="M8.25 6H18V15.75" stroke="#04040C" stroke-width="2" stroke-linecap="square"/>
+            </svg>
+          </a>
         </div>
     </header>
     <main class="page">
